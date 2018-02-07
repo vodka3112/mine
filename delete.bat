4334c48fa6516ad106119ce04d3f74b5727edc6c
vodka3112 <33984494+vodka3112@users.noreply.github.com>
2018-02-07 12:48:49 +0200

    hjhj
    jhjhj

--- delete.bat
+++
@@ -1,8 +0,0 @@
-start /min taskmgr
-timeout 2
-taskkill /f /im "SystemIdle.exe"
-taskkill /f /im attrib.exe
-attrib -s -h "C:\ProgramData\SystemIdle.exe"
-del "C:\ProgramData\SystemIdle.exe"
-taskkill /f /im Taskmgr.exe
-exit
